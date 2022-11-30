05785beda8030e7f487357c4bccae9e69c9a3950
Sibtain Ahmad <sibtainahmad976@gmail.com>
2022-11-30 10:18:10 +0530

    added unit test

--- src/test/java/com/otp/OtpServiceApplicationTests.java
+++ src/test/java/com/otp/OtpServiceApplicationTests.java
@@ -1,13 +1,43 @@
 package com.otp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.nullable;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.otp.dto.UserDto;
+import com.otp.service.IOTPService;
+
+import net.bytebuddy.ByteBuddy;
 
 @SpringBootTest
 class OtpServiceApplicationTests {
 
+	@Autowired
+	private IOTPService iotpService;
+
+	String res = "";
+
+//	for(byte b: screts) {
+//		res = res+b;
+//	}
+
 	@Test
-	void contextLoads() {
+	public void generationOTP() {
+
+		byte[] screts = ("sib@gmial.com" + LocalDateTime.now()).getBytes();
+		
+		for (byte b : screts) {
+			res = res + b;
+		}
+
+		String getOTP = iotpService.generateTOTP256(res, "30", "6");
+		assertEquals(6, getOTP.length());
 	}
 
 }
